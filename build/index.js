52aa89593c16154eb0839481da1252a878862408
Watson Chen <wc.watsonchen@gmail.com>
2022-12-21 23:05:32 -0500

    refactor code to use interface approach

--- build/index.js
+++ build/index.js
@@ -2,12 +2,14 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const MatchReader_1 = require("./MatchReader");
 const MatchResults_1 = require("./MatchResults");
-const reader = new MatchReader_1.MatchReader("football.csv");
-reader.read();
-const dateOfFirstMatch = reader.data[0][0];
-console.log(dateOfFirstMatch);
+const CsvFileReader_1 = require("./CsvFileReader");
+//Create an object that satisfies the 'DataReader' interface
+const csvFileReader = new CsvFileReader_1.CsvFileReader('football.csv');
+//Create an instance of MatchReader and pass in something satisfying the 'DataReader' interface
+const matchReader = new MatchReader_1.MatchReader(csvFileReader);
+matchReader.load();
 let manUnitedWins = 0;
-for (let match of reader.data) {
+for (let match of matchReader.matches) {
     if (match[1] === "Man United" && match[5] === MatchResults_1.MatchResult.HomeWin) {
         manUnitedWins++;
     }
